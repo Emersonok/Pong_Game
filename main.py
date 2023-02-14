ea739dde3e31ad3afa8079c78fdb1824a8810706
Emerson Okorie <emersonokorie@icloud.com>
2023-02-13 22:05:57 -0300

    update

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
-from turtle import Turtle, Screen 
+from turtle import Screen 
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+initial_time = 0.1
+
 
 screen = Screen()
+screen.listen()
 screen.bgcolor("black")
 screen.setup(800, 600)
 screen.title("Pong")
-
-paddles = []
-starting_position = [(0,0), (0, 10), (0,20), (0,30), (0,40), (0,50)]
-new_position = [(350,0), (350, 10), (350,20), (350,30), (350,40), (350,50)]
-
-def up_movement():
-        paddle.setheading(90)
-        paddle.forward(20)
-def down_movement():
-        paddle.setheading(270)
-        paddle.forward(20)
-
-for blocks in range (6):
-        paddle = Turtle("square")
-        paddle.penup()
-        paddle.color("white")
-        paddle.goto(starting_position[blocks])
-        paddle.goto(new_position[blocks])
-        paddles.append(paddle)
+screen.tracer(0)
 
 
-screen.listen()
-screen.onkey(up_movement, "Up")
-screen.onkey(down_movement, "Down")
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+scoreboard = Scoreboard()
 
+ball = Ball()
 
+screen.onkey(r_paddle.move_up, "Up")
+screen.onkey(r_paddle.move_down, "Down")
+screen.onkey(l_paddle.move_up, "w")
+screen.onkey(l_paddle.move_down, "s")
 
-    
-    
+game_on = True
 
+while game_on:
+        time.sleep(ball.initial_speed)
+        screen.update()
+        ball.ball_move()
+
+        #detect collision
+        if ball.ycor() > 280 or ball.ycor() < -280:
+                ball.bounce()
+
+        if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+                ball.bounce_x()
+                
+                
+                
+
+        if ball.xcor() > 380:
+                ball.reset()
+                scoreboard.l_point()
+        if ball.xcor() < -380:
+                ball.reset()
+                scoreboard.r_point()
+
+        #if scoreboard.l_point() == 3 or scoreboard.r_point() == 3:
+              #  game_on = False
+              #  print("Game Over")
+                
+        
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,21 +1,23 @@
 from turtle import Turtle
-paddles = []
-starting_position = [(0,0), (0, 10), (0,20), (0,30), (0,40), (0,50)]
-new_position = [(350,0), (350, 10), (350,20), (350,30), (350,40), (350,50)]
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(5, 1)
+        self.penup()
+        self.goto(position)
+
+    def move_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y )
+
+    def move_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y )
+    
+    
 
 
-class Paddle:
-    for blocks in range (6):
-        paddle = Turtle("square")
-        paddle.penup()
-        paddle.color("white")
-        paddle.goto(starting_position[blocks])
-        paddle.goto(new_position[blocks])
-        paddles.append(paddle)
-
-    def up_movement():
-        paddles.setheading(90)
-        paddles.forward(20)
-    def down_movement():
-        paddles.setheading(270)
-        paddles.forward(20)
+    
